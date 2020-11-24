64dc356096936932c526e9c85685446d780806f8
Bob <smilewalkman@sina.com>
2020-11-24 13:54:34 +0800

    添加不同功能

--- Stock/src/history/price/ReadCSV.java
+++ Stock/src/history/price/ReadCSV.java
@@ -11,7 +11,14 @@ public class ReadCSV {
 	static int satifyNum = 0;
 	static int totalNum = 0;
 
-	public static int readCSV(String filePath, int count, double percent) {
+	/**
+	 * 一个股票前面连续多少天涨幅大于某个值有多少个
+	 * @param filePath
+	 * @param count 前面连续多少个满足条件
+	 * @param percent 涨幅大于多少为满足条件
+	 * @return
+	 */
+	public static int fun1(String filePath, int count, double percent) {
 		satifyNum = 0;
 		totalNum = 0;
 		double previous[] = new double[count];
@@ -58,19 +65,85 @@ public class ReadCSV {
 		}
 		System.out.println(filePath + ":" + satifyNum + "/" + totalNum);
 		return satifyNum;
-
 	}
-
-	public static void main(String[] args) {
+	public static void fun1_percent() {
 		String dir[] = new File("historyprice").list();
-//		String dir[] = { "600180_historyprice.xls" };
 		int total = 0;
 		int num = 0;
 		for (int i = 0; i < 2000 && i < dir.length; i++) {
-			int n = ReadCSV.readCSV("historyprice/" + dir[i],3, 0.0);
+			int n = ReadCSV.fun1("historyprice/" + dir[i],3, 0.0);
 			total += totalNum;
 			num += satifyNum;
 		}
 		System.out.println(num + "/" + total + ":" + num / total);
+	}
+	
+	/**
+	 * 一个股票，当天涨幅大于多少值，前面多少天内有多少天有涨，
+	 * e.g. 今天是第N个交易日涨幅大于P%，N-M到N-1天里有多少天是涨的
+	 * @param filePath
+	 * @param count 前面连续多少个满足条件
+	 * @param percent 涨幅大于多少为满足条件
+	 * @return
+	 */
+	public static int fun2(String filePath, int N, int M, double P) {
+		satifyNum = 0;
+		totalNum = 0;
+		double previous[] = new double[N];
+		ArrayList<Double> ar = new ArrayList<Double>();
+		ArrayList<String> dateAr = new ArrayList<String>();
+		try {
+			// 创建CSV读对象
+			CsvReader csvReader = new CsvReader(filePath);
+
+			// 读表头
+			csvReader.readHeaders();
+			while (csvReader.readRecord()) {
+//				// 读一整行
+//				System.out.println(csvReader.getRawRecord());
+//				// 读这行的某一列
+//				System.out.println("涨跌幅："+csvReader.get(9));
+				if (!"None".equals(csvReader.get(9))) {
+					ar.add(Double.parseDouble(csvReader.get(9)));
+					dateAr.add(csvReader.get(0));
+				}
+			}
+			for(int i=0;i<ar.size()-N-1;i++) {
+				double current=ar.get(i);
+				if(current>=P) {
+					totalNum++;
+					int sn=0;
+					for(int k=i+N;k>i;k--) {
+						double pCurrent=ar.get(k);
+						if(pCurrent>0) {
+							sn++;
+						}
+					}
+					if(sn>=M) {
+						satifyNum++;
+					}
+				}
+			}
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(filePath + ":" + satifyNum + "/" + totalNum);
+		return satifyNum;
+	}
+	public static void fun2_percent() {
+		String dir[] = new File("historyprice").list();
+		int total = 0;
+		int num = 0;
+		for (int i = 0; i < 2000 && i < dir.length; i++) {
+			int n = ReadCSV.fun2("historyprice/" + dir[i],10, 5, 5.0);
+			total += totalNum;
+			num += satifyNum;
+		}
+		System.out.println(num + "/" + total + ":" + (double)num / (double)total);
+	}
+	public static void main(String[] args) {
+		fun2_percent();
 	}
 }
